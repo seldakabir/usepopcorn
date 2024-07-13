a3c16f801c3ea1b3cd7bd83f4cfa4fb1c5d9d54d
selda kabir <seldaakabirnejat@gmail.com>
2024-07-13 19:54:51 +0300

    addWatched

--- src/App.js
+++ src/App.js
@@ -61,6 +61,9 @@ export default function App() {
   function handleNullId() {
     setSelectedId(null)
   }
+  function handleAddWatchedMovies(movie) {
+    setWached(watched=>[...watched,movie])
+  }
 
   useEffect( function () {
     async function fetchMovies() {
@@ -113,7 +116,7 @@ export default function App() {
         {errorMessage && <Message message={errorMessage} />}
       </Box>
       <Box data={watched}>
-        {selectedId ? <MovieDetails id={selectedId} onHanldeId={handleId} handleNullId={handleNullId} /> :
+        {selectedId ? <MovieDetails id={selectedId} onHanldeId={handleId} handleNullId={handleNullId} onAddWatchedMovie={handleAddWatchedMovies } /> :
 
           <>
         <Summery data={watched} />
@@ -185,7 +188,7 @@ function MovieList({data,id,onHandleId}) {
    ))}
     </ul>
 }
-function MovieDetails({ id, handleNullId }) {
+function MovieDetails({ id, handleNullId,onAddWatchedMovie }) {
   const [movie, setMovie] = useState({})
   const [isLoading, setIsloading] = useState(false)
   const[isError,setError]=useState('')
@@ -201,7 +204,15 @@ function MovieDetails({ id, handleNullId }) {
     Director: director,
     Genr: genre
   } = movie
+  function handleAdd() {
+
+    const newMovie = {
   
+}
+
+
+    onAddWatchedMovie(newMovie)
+  }
   useEffect(function () {
     async function getMoviesDetails() {
       setIsloading(true)
@@ -240,10 +251,14 @@ if(!res.ok) throw new Error('there is an error')
         </p>
       </div>
     </header>
-    <section>
+      <section>
+        <div className="rating">
+          <StarRating />
+          <button className="btn-add " >+ Add to list</button>
+          </div>
       <p><em>{plot}</em></p>
       <p>Starring {actors}</p>
-      <p>Directed by<b>{director}</b></p>
+      <p>Directed by<b> {director}</b></p>
       
       </section>
       </>
@@ -311,4 +326,68 @@ function Watched({ data }) {
       </li>
    ))} 
       </ul>
+}
+
+function StarRating() {
+  const [rating, setRating] = useState(0)
+  const[tempRating,setTempRating]=useState(0)
+ 
+  return<div style={container}>
+  <div style={starContainer}>
+    {
+        Array.from({ length: 10 }, (_, i) => <Star key={i + 1} onClick={() => setRating(i + 1)} onHover={() => setTempRating(i + 1)}
+           onLeave={()=>setTempRating(0)}
+          full={tempRating?tempRating>=i+1:rating>=i+1}
+        />)
+    }
+    </div>
+    <p>{tempRating || rating ||''}</p>
+    </div>
+}
+const starStyle = {
+  width: '24px',
+  height: '24px',
+  display: 'flex',
+ 
+}
+const starContainer = {
+  display: 'flex',
+  justifyContent: ' center',
+  alignItems: 'center',
+  
+}
+const container = {
+  display: 'flex',
+  justifyContent: ' center',
+  alignItems: 'center',
+    gap:'2rem'
+}
+
+
+function Star({onClick,full,onHover}) {
+  return <span style={starStyle} onClick={onClick} onMouseEnter={onHover}>
+    {full?<svg
+  xmlns="http://www.w3.org/2000/svg"
+  viewBox="0 0 20 20"
+  fill="#fbf204"
+  stroke="#fbf204"
+>
+  <path
+    d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"
+  />
+</svg>:<svg
+  xmlns="http://www.w3.org/2000/svg"
+  fill="none"
+  viewBox="0 0 24 24"
+  stroke="#fbf204"
+>
+  <path
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    strokeWidth="{2}"
+    d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z"
+  />
+</svg>
+}
+  </span>
 }
