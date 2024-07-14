0a410546ae0a97d5e107d74dbd2f1f2d072399f5
selda kabir <seldaakabirnejat@gmail.com>
2024-07-15 01:11:44 +0300

    fixTitle

--- src/App.js
+++ src/App.js
@@ -260,6 +260,10 @@ if(!res.ok) throw new Error('there is an error')
     }
     getMoviesDetails()
   }, [id])
+  useEffect(function () {
+    if (!title) return;
+    document.title=`Movie |${title}`
+  },[title])
   return  <div className="details"  >
    { isLoading && <Loading />}
   {isError && <Message/>}
